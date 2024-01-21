c4fed137c2fbaf340a53fc35d3ce76558081d27e
twigs <hugo.pitorro@tum.de>
2024-01-21 01:45:23 +0000

    revert route upsert issue

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,8 +1,9 @@
-import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import { getFrontendURL } from "@/lib/utils";
+
+import { getSupabaseRouteClient } from "@/lib/supabase_utils";
+
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
@@ -10,17 +11,30 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  const header = request.headers;
 
   if (code) {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-    const data = await supabase.auth.exchangeCodeForSession(code);
+    const supabase = getSupabaseRouteClient();
 
-    if (data.error !== null) {
-      console.error("Error exchanging auth code for session:", data.error);
-      return NextResponse.error();
+    const {
+      data: { user },
+    } = await supabase.auth.exchangeCodeForSession(code);
+
+    const userId = user?.id;
+    const name = user?.user_metadata?.name;
+
+    const { data, error } = await supabase
+      .from("user_data")
+      .upsert({ id: userId, name: name, usage: 0 }, { ignoreDuplicates: true })
+      .select();
+
+    // FIXME for some reasone error is an empty object instead of null
+    if (error !== null) {
+      console.log(error);
+      return NextResponse.json({ error: error?.message }, { status: 500 });
     }
   }
 
-  redirect(getFrontendURL()!);
+  // URL to redirect to after sign up process completes
+  return NextResponse.redirect(new URL(getFrontendURL()!), { status: 303 });
 }
