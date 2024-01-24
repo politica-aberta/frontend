831b98bdca488cbdb293f463afe1ad993d14eb4b
José Martinho <jose.m.oliveiramartinho@gmail.com>
2024-01-24 22:17:58 +0000

    small changes

--- components/DonationsButton.tsx
+++ components/DonationsButton.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 interface DonationsButton extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function DonationsButton() {
-    const handleRedirect = () => {window.location.replace("https://donate.stripe.com/test_28o6rJ3LUg6H3JKdQQ")}
+    const handleRedirect = () => {window.location.replace("https://donate.stripe.com/3csbLi47Rb5S8AU144")}
 
     return <Button onClick={handleRedirect} className="flex flex-col">
             Faz uma doação ❤️
