541a7cf33d8b504c12d95e874c5a16ae4d3d2c86
José Martinho <jose.m.oliveiramartinho@gmail.com>
2024-01-26 00:50:34 +0000

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://politica-aberta.pt',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1,
+        },
+        {
+            url: 'https://politica-aberta.pt/about',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.5,
+        },
+        {
+            url: 'https://politica-aberta.pt/chat',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8,
+        },
+        {
+            url: 'https://politica-aberta.pt/auth/sign-up',
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8,
+        },
+    ]
+}
