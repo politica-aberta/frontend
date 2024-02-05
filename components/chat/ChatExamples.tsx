eb328e7a5932d881f17533210697dfb40398d18d
José Martinho <jose.m.oliveiramartinho@gmail.com>
2024-02-05 01:39:38 +0000

    update random examples

--- components/chat/ChatExamples.tsx
+++ components/chat/ChatExamples.tsx
@@ -1,15 +1,65 @@
-import { FC } from "react";
+"use client";
 
+import React, { FC, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "../ui/button";
+import { parties } from "@/lib/constants";
 
 interface ChatExamplesProps extends React.HTMLAttributes<HTMLDivElement> {
   party: string;
   setInput: (input: string) => void;
 }
 
+function pickRandomTwo(array: string[]) {
+  const index1 = Math.floor(Math.random() * array.length);
+  let index2 = Math.floor(Math.random() * array.length);
+  // Ensure index2 is different from index1
+  while (index2 === index1) {
+    index2 = Math.floor(Math.random() * array.length);
+  }
+  return [array[index1], array[index2]];
+}
+
 const ChatExamples: FC<ChatExamplesProps> = ({ className, ...props }) => {
-  // TODO set up probabilistic rendering?
+  const [randParties, setRandParties] = useState<string[][]>([]);
+  const [comparisonExamples, setComparisonExamples] = useState<{prob: number, question: string}[]>([]);
+
+  useEffect(() => {
+    const newRandParties = [
+      pickRandomTwo(parties.map((v) => v.id.toUpperCase())),
+      pickRandomTwo(parties.map((v) => v.id.toUpperCase())),
+      pickRandomTwo(parties.map((v) => v.id.toUpperCase())),
+      pickRandomTwo(parties.map((v) => v.id.toUpperCase())),
+    ];
+    setRandParties(newRandParties);
+  }, []);
+
+  useEffect(() => {
+    if(randParties && randParties.length > 0) {
+
+      setComparisonExamples(
+        [{
+          prob: 0.8,
+          question: `Como pretendem o ${randParties[0][0]} e o ${randParties[0][1]} abordar a questão das mudanças climáticas?`,
+        },
+        {
+          prob: 0.55,
+          question: `Qual é a visão dos ${randParties[1][0]} e da ${randParties[1][1]} sobre a geração de empregos e crescimento económico?`,
+        },
+        {
+          prob: 0.6,
+          question: `Quais são as estratégias do ${randParties[2][0]} e do ${randParties[2][1]} para melhorar o SNS?`,
+        },
+        {
+          prob: 0.7,
+          question: `Em que aspectos as políticas de combate à corrupção diferem entre o ${randParties[3][0]} e o ${randParties[3][1]}?`,
+        },]
+      )
+    }
+  }, [randParties]);
+
+
+
   const examples = [
     {
       prob: 0.3,
@@ -28,33 +78,15 @@ const ChatExamples: FC<ChatExamplesProps> = ({ className, ...props }) => {
       question: `Quais são as medidas do ${props.party} para combater a corrupção?`,
     },
   ];
-  const comparisonExamples = [
-    {
-      prob: 0.8,
-      question: `Como pretendem o PAN e o LIVRE abordar a questão das mudanças climáticas?`,
-    },
-    {
-      prob: 0.55,
-      question: `Qual é a visão dos PS e da IL sobre a geração de empregos e crescimento económico?`,
-    },
-    {
-      prob: 0.6,
-      question: `Quais são as estratégias dos partidos para melhorar o sistema de saúde pública?`,
-    },
-    {
-      prob: 0.7,
-      question: `Em que aspectos as políticas de combate à corrupção diferem entre os partidos?`,
-    },
-  ];
 
   const questionsToDisplay =
     props.party === "MULTI" ? comparisonExamples : examples;
 
   return (
     <>
-      <h2 className="text-description pl-4 pb-1">Exemplos</h2>
+      {questionsToDisplay && questionsToDisplay.length > 0 &&<h2 className="text-description pl-4 pb-1">Exemplos</h2>}
       <div className={cn("grid lg:grid-cols-2 gap-4", className)}>
-        {questionsToDisplay.map((ex, index) => (
+        {questionsToDisplay && questionsToDisplay.length > 0 && questionsToDisplay.map((ex, index) => (
           <Button
             key={index} // Don't forget to add a key when mapping in React
             className={`h-20 p-4 text-left whitespace-normal items-start justify-start ${
